3c6b9e8b00892cf073885bbca625eddf59fb60bf
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-20 17:35:43 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
 import './App.css'
+import LandingPage from './LandingPage'
 
-function App() {
-  return (
-    <div>
-      <header>Header</header>
-      <main>Main</main>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>Header</header>
+        <LandingPage/>
+        
+        <main>Main</main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/BookSearch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class BookSearch extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Book Search</h1>
+            </div>
+        )
+    }
+}
+
