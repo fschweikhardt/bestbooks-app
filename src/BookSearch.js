5dd97e332faac2e49183b7771ccf8f7e29705782
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-20 23:40:40 -0400

    Update BookSearch.js

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,41 +1,65 @@
 import React from 'react'
+import DATA from './Database'
 
 export default class BookSearch extends React.Component {
+
+    handleSubmit = e => {
+        e.preventDefault()
+        // if (e.target.list1.checked == true) return alert('checked')
+        console.log(e.target.list1.checked)
+        console.log(e.target.year.value)
+        console.log(e.target.author.value)
+    }
 
     render() {
         return (
             <div>
                 <hr />
                 <h1>Book Search</h1>
-                <form onSubmit={this.submitForm}>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <h3>Award Lists</h3>
-                        <label>
-                            Lists 1
+                        <br />
+                        <label htmlFor="The Booker Prize">
+                            The Booker Prize
                             <input
-                                type="checkbox"/>
+                                type="checkbox"
+                                name="The Booker Prize"
+                                id="list1"
+                                value="include"
+                            />
                         </label>
-                        <label>
-                            Lists 2
+                        <br />
+                        <label htmlFor="National Book Award for Fiction">
+                            National Book Award for Fiction
                             <input
-                                type="checkbox"/>
+                                type="checkbox"
+                                name="National Book Award for Fiction"
+                                id="list2"
+                                value="National Book Award for Fiction"
+                            />
                         </label>
-                        <label>
-                            Lists 3
+                        <br />
+                        <label htmlFor="The Pultizer Prize for Fiction">
+                            The Pultizer Prize for Fiction
                             <input
-                                type="checkbox"/>
+                                type="checkbox"
+                                name="The Pultizer Prize for Fiction"
+                                id="list3"
+                                value="The Pultizer Prize for Fiction"
+                            />
                         </label>
+                        <br />
                     </div>
                     <div>
                         <h3>Year</h3>
                         <label htmlFor="year">
                             Choose a year
-                            <select name="year">
+                            <select name="year" id="year">
+                                <option hidden disabled selected value></option>
                                 <option value="2020">2020</option>
                                 <option value="2019">2019</option>
                                 <option value="2018">2018</option>
-                                <option value="2017">2017</option>
-                                <option value="2016">2016</option>
                             </select>
                         </label>
                     </div>
@@ -46,7 +70,9 @@ export default class BookSearch extends React.Component {
                             <input 
                                 type="search" 
                                 name="author"
-                                id="author" />
+                                id="author" 
+                                defaultValue="dan"
+                            />
                         </label>
                     </div>
                     <br />
